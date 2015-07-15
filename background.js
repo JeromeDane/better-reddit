9b75d4fe7a785aa878a6f3b82c32aef2ab3ec5f4
Jerome Dane <jerome.dane@gmail.com>
2015-07-15 08:38:59 -0400

    v1.2

--- background.js
+++ background.js
@@ -1,15 +1,41 @@
+function getScript(path, callback) {
+	var url = chrome.extension.getURL(path);
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status==200) {
+			console.log('loaded ' + path);
+			callback(xmlhttp.responseText);
+		}
+	};
+	xmlhttp.open("GET",url,false);
+	xmlhttp.send();
+}
+
+function getScripts(scripts, callback) {
+	var numLoaded = 0;
+	var source = "";
+	for(var i = 0; i < scripts.length; i++) {
+		getScript(scripts[i], function(code) {
+			source += code;
+			numLoaded++;
+			if(numLoaded == scripts.length)
+				callback(source);
+		});
+	}
+}
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
 	switch(request.name) {
 		case 'getsource':
-			var url = chrome.extension.getURL('redditplus.userscript.js');
-			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.onreadystatechange = function() {
-				if (xmlhttp.readyState == 4 && xmlhttp.status==200) {
-					sendResponse(xmlhttp.responseText);
-				}
-			};
-			xmlhttp.open("GET",url,true);
-			xmlhttp.send();
+			var scripts = [
+				'jquery.js',
+				'jqueryui.js',
+				'masonry.js',
+				'redditplus.userscript.js'
+			];
+			getScripts(scripts, function(source) {
+				sendResponse('(function() { ' + source + '})()');
+			});
 			break;
 	}
 });
